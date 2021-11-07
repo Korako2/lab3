67584b0e1e24ef854e70f21d9d2d3081ec5afdae
Korako2 <Korako2@mail.ru>
2021-11-07 17:01:22 +0300

    Add comments

--- src/lab3/Main.java
+++ src/lab3/Main.java
@@ -1,7 +1,44 @@
 package lab3;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int randomMoney;
+        boolean resultOfObjectRotation;
+        boolean resultOfKnock;
+        double randomResultOfObjectRotation;
+        double randomResultKnock;
+        while (true) {
+            System.out.println("Если вы хотите увидеть исходную историю введите 1, если хотите увидеть случайную историю введите 0");
+            float number = scanner.nextFloat();
+            if (number == 0f) {
+                int k = 6; // коэффициент для генерации рандомного числа
+                randomMoney = (int) Math.round(k * Math.random()); // генерация денег Козлика
+                randomResultOfObjectRotation = Math.random();
+                randomResultKnock = Math.random();
+                if (randomResultOfObjectRotation >= 0.5) {
+                    resultOfObjectRotation = true;
+                } else {
+                    resultOfObjectRotation = false;
+                }
+                if (randomResultKnock >= 0.5) {
+                    resultOfKnock = true;
+                } else {
+                    resultOfKnock = false;
+                }
+                break;
+            }
+            if (number == 1f) {
+                randomMoney = 4;
+                resultOfKnock = false;
+                resultOfObjectRotation = false;
+                break;
+            } else {
+                System.out.println("Вы ввели неверное число, давайте попробуем заново.");
+            }
+        }
         Room room = new Room("комната", 16);
         Sink sink = new Sink(new Faucet("кран"), "рукомойник");
         Lamp lamp = new Lamp("лампочка");
@@ -19,7 +56,8 @@ public class Main {
         acceptingMoneyMachineForLamp.setLocation(room);
         room.getInformationAboutThingsInTheRoom();
 
-        MoonShorty kozlic = new MoonShorty("Козлик", 20, 4);
+
+        MoonShorty kozlic = new MoonShorty("Козлик", 20, randomMoney);
         EarthShorty neznaika = new EarthShorty("Незнайка", 16);
         kozlic.setLocation(room);
         neznaika.setLocation(room);
@@ -27,6 +65,9 @@ public class Main {
         kozlic.comeToSomething(sink);
         boolean flag2 = false;
         boolean flag3 = false;
+
+
+
         boolean flag1 = kozlic.payForSomething(acceptingMoneyMachineForSoap);
         if (flag1) flag2 = kozlic.payForSomething(acceptingMoneyMachineForTowel);
         if (flag2) flag3 = kozlic.payForSomething(acceptingMoneyMachineForFaucet);
@@ -34,8 +75,8 @@ public class Main {
             if (kozlic.washUp(acceptingMoneyMachineForSoap, acceptingMoneyMachineForTowel, acceptingMoneyMachineForFaucet)) {
                 neznaika.startToDoSomething("умываться");
                 if (!neznaika.washUp(acceptingMoneyMachineForSoap, acceptingMoneyMachineForTowel, acceptingMoneyMachineForFaucet)) {
-                    if (!neznaika.turn(sink.getFaucet(), MethodsOfObjectRotation.BACKANDFORTH, false)) {
-                        if (!neznaika.knock(sink.getFaucet(), false)) {
+                    if (!neznaika.turn(sink.getFaucet(), MethodsOfObjectRotation.BACKANDFORTH, resultOfObjectRotation)) {
+                        if (!neznaika.knock(sink.getFaucet(), resultOfKnock)) {
                             soap.pinchEyes(neznaika, "нет воды");
                             neznaika.callForHelp(kozlic);
                             kozlic.analyzeTheSituation("что-то неладное");
